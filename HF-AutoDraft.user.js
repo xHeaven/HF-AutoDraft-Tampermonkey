d1530c4cc23f4cf47c3b126b9224d8ade454c959
Mark <14284867+xHeaven@users.noreply.github.com>
2022-05-17 13:07:31 +0200

    Create HF-AutoDraft.user.js

---
+++ HF-AutoDraft.user.js
@@ -0,0 +1,108 @@
+// ==UserScript==
+// @name         HF-AutoDraft
+// @namespace    https://hackforums.net/
+// @version      1.0
+// @description  HF automatic draft utility
+// @author       xHeaven#2143
+// @match        *://hackforums.net/showthread.php*
+// @match        *://hackforums.net/newreply.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	const qs = (selector, parent = document) => {
+		return parent.querySelector(selector);
+	}
+
+	const isMoreThanOneDay = (date1, date2) => {
+		const diff = Math.abs(date2.getTime() - date1.getTime());
+		const diffDays = Math.ceil(diff / (1000 * 3600 * 24));
+		return diffDays > 1;
+	}
+
+	const debounce = (func, timeout = 300) => {
+		let timer;
+		return (...args) => {
+			clearTimeout(timer);
+			timer = setTimeout(() => {
+				func.apply(this, args);
+			}, timeout);
+		}
+	}
+
+	let messageBox, tid;
+	const pathName = window.location.pathname;
+	const urlParams = new URLSearchParams(window.location.search);
+
+	const init = () => {
+		if (!urlParams.has('tid')) {
+			return false;
+		}
+
+		tid = urlParams.get('tid');
+
+		if (pathName === "/showthread.php") {
+			messageBox = qs("textarea[name='message'][id='message']");
+		}
+
+		if (pathName === "/newreply.php") {
+			messageBox = qs("textarea:not(#message)");
+		}
+
+		return !(!messageBox || !tid);
+	}
+
+	const restore = () => {
+		const draftRaw = localStorage.getItem(`hackforums-auto-draft-${tid}`);
+		const draft = JSON.parse(draftRaw);
+
+		if (!draft) {
+			return;
+		}
+
+		if (!draft.date || !draft.message || draft.message === "") {
+			return;
+		}
+
+		const draftDate = new Date(draft.date);
+
+		if (isNaN(draftDate.getTime())) {
+			return;
+		}
+
+		if (isMoreThanOneDay(new Date(), draftDate)) {
+			console.log(`Draft is more than one day old, not restoring. Removing it from cache... - ${tid}`);
+			localStorage.removeItem(`hackforums-auto-draft-${tid}`);
+			console.log(`Removed! - ${tid}`);
+		} else {
+			messageBox.value = draft.message;
+			console.log(`Draft restored! - ${tid}`);
+		}
+	}
+
+	const listen = () => {
+		messageBox.addEventListener('keyup', debounce(() => {
+			const draftObj = {
+				message: messageBox.value, date: new Date()
+			}
+
+			const draft = JSON.stringify(draftObj);
+
+			localStorage.setItem(`hackforums-auto-draft-${tid}`, draft);
+
+			console.log(`Draft saved! - ${tid}`);
+		}));
+	}
+
+	if (!init()) {
+		console.log(`HF Auto-Draft init failed. - ${tid}`);
+		return;
+	}
+
+	restore();
+
+	listen();
+})();
